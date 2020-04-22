e86eb075d2258787852770cae1eef377ddbb78b7
Ian Saini <ian.saini@tcs.com>
2020-04-22 15:58:25 -0700

    image slider and weather now on home page

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,33 @@
 const express = require("express");
 const Router = express.Router();
+const NewsModel = require("../model/news");
+const { checkNewstitle } = require("../validator")
 
 Router.get("/", (req, res) => {
-    res.send([])
-})
+    NewsModel.find().then((response)=>{
+        res.render("home", { posts : response });
+    })
+});
+
+Router.post("/", (req, res)=>{
+
+    if(req.body.title !== "" && checkNewstitle(req.body.title) &&  req.body.description !== "" && req.body.url !== "" && req.body.urltoimage !== "")
+        {
+            const News = new NewsModel({
+                title : req.body.title.trim(),
+                description : req.body.description.trim(),
+                url : req.body.url.trim(),
+                urltoimage : req.body.urltoimage.trim()
+            })
+            News.save();
+            res.send("news has been added...");
+        }
+    else
+        {
+            res.send("input is invalid.");
+        }
+});
+
+module.exports = Router;
+
 module.exports = Router;
--- routes/weather.js
+++ routes/weather.js
@@ -1,20 +1,11 @@
 const express = require("express");
 const Router = express.Router();
-const UserModel = require("../model/user");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-
-const Config = require("../config")
 
 Router.get("/", (req, res) => {
     res.render("weather")
 })
 
-Router.get("/location", (req, res) => {
-    res.render("location")
-})
-
-Router.get("/weather/:lat/:long", (req, res) => {
+Router.get("/:lat/:long", (req, res) => {
     let request = require('request');
 
     let apiKey = 'bd961ca2348c73ad7c90071bbf8739e0';

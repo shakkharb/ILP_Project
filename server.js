69b1d1c791950150265a48c585aea6239ee4914d
Ian Saini <ian.saini@tcs.com>
2020-04-22 18:00:46 -0700

    refix

--- server.js
+++ server.js
@@ -1,11 +1,3 @@
-import express from "express";
-import path from "path";
-import http from "http";
-import socketio from "socket.io";
-
-// const application = express();
-const socketUsers = {};
-///////////////////////////////////////////
 const express = require("express")
 const application = require("./application");
 const mongoose = require("mongoose");
@@ -32,45 +24,9 @@ const port = 9001;
 /////////////////////////////////////////////////////
 
 
-const server = http.createServer(application)(port, ()=>{
+const server = application.listen(port, ()=>{
     console.log(`...Server running on localhost:${port}...`);
 })
 
-/////////////////////////////////////////////////////////////////
-
-import express from "express";
-import path from "path";
-import http from "http";
-import socketio from "socket.io";
-
-const application = express();
-const socketUsers = {};
-
-application.use(express.static(path.join(__dirname, "views")))
-
-const server = http.createServer(application).listen(9002, () => {
-    console.log("...Chat app started on localhost:9002...");
-})
-
-const SocketInstance = socketio.listen(server);
-
-SocketInstance.sockets.on("connection", (socket) => {
-    var list = SocketInstance.sockets.sockets;
-    var user = Object.keys(list);
-
-    socket.on("signin", (name) => {
-        socketUsers[socket] = { name : name }
-        socket.emit("userloggedin", user)
-    })
-
-    socket.on("chat", (message) => {
-        socket.emit("chat", { sendBy : socketUsers[socket], message })
-        socket.broadcast.emit("chat", { sentBy : socketUsers[socket], message })
-    })
-
-})
-
-
-
 require("@babel/register")({ presets : ["@babel/preset-env"] });
-module.exports = require("./application");//////
+module.exports = require("./chatapp");//////

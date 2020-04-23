4bf18db3b9cfb55db02e0d19da2e5677f97b42dd
Ian Saini <ian.saini@tcs.com>
2020-04-22 17:49:40 -0700

    validator fixed

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+import express from "express";
+import path from "path";
+import http from "http";
+import socketio from "socket.io";
+
+// const application = express();
+const socketUsers = {};
+///////////////////////////////////////////
 const express = require("express")
 const application = require("./application");
 const mongoose = require("mongoose");
@@ -24,9 +32,45 @@ const port = 9001;
 /////////////////////////////////////////////////////
 
 
-const server = application.listen(port, ()=>{
+const server = http.createServer(application)(port, ()=>{
     console.log(`...Server running on localhost:${port}...`);
 })
 
+/////////////////////////////////////////////////////////////////
+
+import express from "express";
+import path from "path";
+import http from "http";
+import socketio from "socket.io";
+
+const application = express();
+const socketUsers = {};
+
+application.use(express.static(path.join(__dirname, "views")))
+
+const server = http.createServer(application).listen(9002, () => {
+    console.log("...Chat app started on localhost:9002...");
+})
+
+const SocketInstance = socketio.listen(server);
+
+SocketInstance.sockets.on("connection", (socket) => {
+    var list = SocketInstance.sockets.sockets;
+    var user = Object.keys(list);
+
+    socket.on("signin", (name) => {
+        socketUsers[socket] = { name : name }
+        socket.emit("userloggedin", user)
+    })
+
+    socket.on("chat", (message) => {
+        socket.emit("chat", { sendBy : socketUsers[socket], message })
+        socket.broadcast.emit("chat", { sentBy : socketUsers[socket], message })
+    })
+
+})
+
+
+
 require("@babel/register")({ presets : ["@babel/preset-env"] });
-module.exports = require("./chatapp");//////
+module.exports = require("./application");//////
--- validator.js
+++ validator.js
@@ -3,15 +3,11 @@ function checkNewstitle(value)
         return RegExp().test(value)
     }
 
-module.exports = {
-    checkNewstitle
-}
-
 function checkContactusqemail(value)
     {
         return RegExp().test(value)
     }
 
 module.exports = {
-    checkContactusqemail
+    checkNewstitle, checkContactusqemail
 }
